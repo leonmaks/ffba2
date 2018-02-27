dbdaeb69f9a2a9c7cdcce12368f15a611278c779
Le Ma <le_ma_@mail.ru>
2018-02-27 13:03:46 +0300

    sql folder added

---
+++ sql/cash_record_procs.sql
@@ -0,0 +1,17 @@
+﻿CREATE OR REPLACE FUNCTION ffba_cash_record_set_record_date() RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.record_date IS NULL THEN
+        NEW.record_date := NEW.entry_date;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql
+;
+
+
+DROP TRIGGER ffba_cash_record_set_record_date ON ffba_cash_record;
+
+
+CREATE TRIGGER ffba_cash_record_set_record_date BEFORE INSERT OR UPDATE ON ffba_cash_record
+    FOR EACH ROW EXECUTE PROCEDURE ffba_cash_record_set_record_date()
+;
